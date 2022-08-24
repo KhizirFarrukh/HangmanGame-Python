f21b1625e1d4ae7b8a2bafecf9a4f84be715b66a
Khizir Farrukh <khizirfarrukh@outlook.com>
2022-08-24 12:29:07 +0500

    Hangman Game v0

--- hangman.py
+++ hangman.py
@@ -6,6 +6,11 @@ import time
 nltk.download('brown')
 EnglishWordsList = brown.words()
 
+def HangmanWordPrinter(InputLetterList):
+  print("\t\t", end="")
+  for i in InputLetterList:
+    print(i, end="  ")
+  print()
 
 print("1 - Easy")
 print("2 - Medium")
@@ -17,20 +22,20 @@ wordLengthMin = 0
 wordLengthMax = 0
 if diff == "1":
   wordLengthMin = 3
-  wordLengthMax = 4
-  # between 3 and 4 
+  wordLengthMax = 5
+  # between 3 and 5 
 elif diff == "2":
-  wordLengthMin = 5
-  wordLengthMax = 7
-  # between 5 and 7
+  wordLengthMin = 6
+  wordLengthMax = 9
+  # between 6 and 9
 elif diff == "3":
-  wordLengthMin = 8
-  wordLengthMax = 11
-  # between 8 and 11
+  wordLengthMin = 10
+  wordLengthMax = 14
+  # between 10 and 14
 elif diff == "4":
-  wordLengthMin = 12
+  wordLengthMin = 15
   wordLengthMax = 1000
-  # greater than or equal to 12
+  # greater than or equal to 15
 
 filteredWordsList = [i.lower() for i in EnglishWordsList if len(i) >= wordLengthMin and len(i) <= wordLengthMax and i.isalpha()]
 random.seed(int(time.time()))
@@ -39,4 +44,31 @@ SelectedWordIndex = random.randint(0,len(filteredWordsList)-1)
 HangmanWord = filteredWordsList[SelectedWordIndex]
 InputLetterList = ["_"] * len(HangmanWord)
 AvailableLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-incorrectCounter = 5
+incorrectTriesLimit = 5
+incorrectCounter = 0
+gameOver = False
+print(HangmanWord)
+
+while not gameOver:
+  HangmanWordPrinter(InputLetterList)
+  print("\n\n\t\tYou have " + str(incorrectTriesLimit - incorrectCounter) + " tries left")
+  print("\n\nAvailable letters: " + str(AvailableLetters))
+  charInput = input("\nEnter letter: ")
+  charInput = charInput.lower()
+  if charInput in AvailableLetters:
+    AvailableLetters.remove(charInput)
+    letterFound = False
+    for i in range(len(HangmanWord)):
+      if HangmanWord[i] == charInput:
+        InputLetterList[i] = charInput
+        letterFound = True
+    if "_" not in InputLetterList:
+      gameOver = True
+      print("\n\n\t\tCongratulations!!! YOU WON!")
+      HangmanWordPrinter(InputLetterList)
+    if not letterFound:
+      incorrectCounter += 1
+      print("\n\n\t\tIncorrect Attempt!\n\n")
+    if incorrectCounter == incorrectTriesLimit:
+      print("\n\n\t\tYOU LOST!! GAME OVER!!!")
+      HangmanWordPrinter(InputLetterList)
