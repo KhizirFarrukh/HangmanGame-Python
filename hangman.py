6863fcaca818c4c6d694a43219d4f2435dfcd2f6
Khizir Farrukh <khizirfarrukh@outlook.com>
2022-08-24 11:42:58 +0500

    Added difficulty selection

--- hangman.py
+++ hangman.py
@@ -4,3 +4,29 @@ from nltk.corpus import brown
 nltk.download('brown')
 EnglishWordsList = brown.words()
 
+
+print("1 - Easy")
+print("2 - Medium")
+print("3 - Hard")
+print("4 - Expert")
+
+diff = input("Select difficulty: ")
+wordLengthMin = 0
+wordLengthMax = 0
+if diff == "1":
+  wordLengthMin = 3
+  wordLengthMax = 4
+  # between 3 and 4 
+elif diff == "2":
+  wordLengthMin = 5
+  wordLengthMax = 7
+  # between 5 and 7
+elif diff == "3":
+  wordLengthMin = 8
+  wordLengthMax = 11
+  # between 8 and 11
+elif diff == "4":
+  wordLengthMin = 12
+  wordLengthMax = 1000
+  # greater than or equal to 12
+
